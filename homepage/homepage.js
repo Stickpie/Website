10c84322e2ea5bb155314eaf82ca9d82512da06c
Jingyi-Guo <alisa.jingyi.guo@gmail.com>
2024-10-21 20:30:54 -0400

    Fixed formatting of Home page

---
+++ homepage/homepage.js
@@ -0,0 +1,7 @@
+document.querySelector('.linkdenButton').addEventListener('click', ()=>{
+    window.open('https://www.linkedin.com/in/alisa-guo-15318b22b/', '_blank');
+});
+
+document.querySelector('.githubButton').addEventListener('click', ()=> {
+    window.open('https://github.com/Stickpie', '_blank');
+});
