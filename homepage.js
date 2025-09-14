f4752e2a8660d506cdb333aabaf8ecaa957a07b5
Stickpie <alisa.jingyi.guo@gmail.com>
2025-09-14 14:44:46 -0400

    Revamped basically entire website

--- homepage.js
+++
@@ -1,7 +0,0 @@
-document.querySelector('.linkdenButton').addEventListener('click', ()=>{
-    window.open('https://www.linkedin.com/in/alisa-guo-15318b22b/', '_blank');
-});
-
-document.querySelector('.githubButton').addEventListener('click', ()=> {
-    window.open('https://github.com/Stickpie', '_blank');
-});
